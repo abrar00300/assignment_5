2d7dafdb6b675a0037309c0fa0fcb8e0a6aa2cd6
abrar00300 <109073654+abrar00300@users.noreply.github.com>
2022-07-19 18:21:25 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\nEnter number=");
+    scanf("%d",&n);
+    printf("\nFirst %d even Natural numbers are=",n);
+    for(int i=1;i<=n;i++)
+        printf("%d ",2*i);
+    return 0;
+}
